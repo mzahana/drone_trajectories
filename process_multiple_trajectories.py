f0278f58dcb2324970a90527784a86150875e021
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2023-09-06 14:02:09 +0300

    Compute spline data and add it to the dataset

--- process_multiple_trajectories.py
+++ process_multiple_trajectories.py
@@ -2,6 +2,7 @@ import pandas as pd
 import argparse
 import numpy as np
 import os
+from scipy.interpolate import splrep
 
 def read_trajectory(file_path):
     df = pd.read_csv(file_path)
@@ -10,6 +11,10 @@ def read_trajectory(file_path):
 def process_trajectory(df, inp_seg_len, out_seg_len, compute_velocity=False):
     input_segments = []
     output_segments = []
+    knots_input = []
+    coeffs_input = []
+    knots_output = []
+    coeffs_output = []
     
     if compute_velocity:
         # Compute velocity (difference in position / difference in time)
@@ -23,10 +28,24 @@ def process_trajectory(df, inp_seg_len, out_seg_len, compute_velocity=False):
         input_segments.append(input_segment)
         output_segments.append(output_segment)
         
+        # Compute spline representation for the input segment
+        tck_x_input = splrep(np.arange(inp_seg_len), input_segment[0], k=3)
+        tck_y_input = splrep(np.arange(inp_seg_len), input_segment[1], k=3)
+        tck_z_input = splrep(np.arange(inp_seg_len), input_segment[2], k=3)
+        knots_input.append(tck_x_input[0])
+        coeffs_input.append(tck_x_input[1])
+        
+        # Compute spline representation for the output segment
+        tck_x_output = splrep(np.arange(out_seg_len), output_segment[0], k=3)
+        tck_y_output = splrep(np.arange(out_seg_len), output_segment[1], k=3)
+        tck_z_output = splrep(np.arange(out_seg_len), output_segment[2], k=3)
+        knots_output.append(tck_x_output[0])
+        coeffs_output.append(tck_x_output[1])
+        
     input_segments = np.array(input_segments).transpose(1, 2, 0)
     output_segments = np.array(output_segments).transpose(1, 2, 0)
     
-    return input_segments, output_segments
+    return input_segments, output_segments, knots_input, coeffs_input, knots_output, coeffs_output
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process drone trajectory data.')
@@ -46,15 +65,23 @@ if __name__ == "__main__":
     
     all_input_segments = []
     all_output_segments = []
+    all_knots_input = []
+    all_coeffs_input = []
+    all_knots_output = []
+    all_coeffs_output = []
     
     for filename in os.listdir(dir_path):
         if filename.endswith('.csv') or filename.endswith('.txt'):
             file_path = os.path.join(dir_path, filename)
             df = read_trajectory(file_path)
-            input_segments, output_segments = process_trajectory(df, inp_seg_len, out_seg_len, compute_velocity)
+            input_segments, output_segments, knots_input, coeffs_input, knots_output, coeffs_output = process_trajectory(df, inp_seg_len, out_seg_len, compute_velocity)
             
             all_input_segments.append(input_segments)
             all_output_segments.append(output_segments)
+            all_knots_input.extend(knots_input)
+            all_coeffs_input.extend(coeffs_input)
+            all_knots_output.extend(knots_output)
+            all_coeffs_output.extend(coeffs_output)
     
     all_input_segments = np.concatenate(all_input_segments, axis=2)
     all_output_segments = np.concatenate(all_output_segments, axis=2)
@@ -65,11 +92,22 @@ if __name__ == "__main__":
     print(f"Total number of input segments: {num_input_segments}")
     print(f"Total number of output segments: {num_output_segments}")
     
-    # Save segments and additional information to .npz file
-    np.savez(save_path, 
-             input_segments=all_input_segments, 
-             output_segments=all_output_segments, 
-             num_input_segments=num_input_segments, 
-             num_output_segments=num_output_segments,
-             inp_seg_len=inp_seg_len,
-             out_seg_len=out_seg_len)
+    # Save segments, spline data, and additional information to .npz file
+    npz_data = {
+        'input_segments': all_input_segments,
+        'output_segments': all_output_segments,
+        'num_input_segments': num_input_segments,
+        'num_output_segments': num_output_segments,
+        'inp_seg_len': inp_seg_len,
+        'out_seg_len': out_seg_len
+    }
+    
+    for i in range(num_input_segments):
+        npz_data[f'knots_input_{i}'] = all_knots_input[i]
+        npz_data[f'coeffs_input_{i}'] = all_coeffs_input[i]
+        
+    for i in range(num_output_segments):
+        npz_data[f'knots_output_{i}'] = all_knots_output[i]
+        npz_data[f'coeffs_output_{i}'] = all_coeffs_output[i]
+    
+    np.savez(save_path, **npz_data)
--- process_single_trajectory.py
+++ process_single_trajectory.py
@@ -1,9 +1,7 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import argparse
 import numpy as np
-import os
+from scipy.interpolate import splrep
 
 def read_trajectory(file_path):
     df = pd.read_csv(file_path)
@@ -12,6 +10,8 @@ def read_trajectory(file_path):
 def process_trajectory(df, inp_seg_len, out_seg_len, compute_velocity=False):
     input_segments = []
     output_segments = []
+    spline_data_input = []
+    spline_data_output = []
     
     if compute_velocity:
         # Compute velocity (difference in position / difference in time)
@@ -25,10 +25,28 @@ def process_trajectory(df, inp_seg_len, out_seg_len, compute_velocity=False):
         input_segments.append(input_segment)
         output_segments.append(output_segment)
         
+        # Compute spline representation for the input segment
+        tck_x_input = splrep(np.arange(inp_seg_len), input_segment[0], k=3)
+        tck_y_input = splrep(np.arange(inp_seg_len), input_segment[1], k=3)
+        tck_z_input = splrep(np.arange(inp_seg_len), input_segment[2], k=3)
+        spline_data_input.append((tck_x_input, tck_y_input, tck_z_input))
+        
+        # Compute spline representation for the output segment
+        tck_x_output = splrep(np.arange(out_seg_len), output_segment[0], k=3)
+        tck_y_output = splrep(np.arange(out_seg_len), output_segment[1], k=3)
+        tck_z_output = splrep(np.arange(out_seg_len), output_segment[2], k=3)
+        spline_data_output.append((tck_x_output, tck_y_output, tck_z_output))
+        
     input_segments = np.array(input_segments).transpose(1, 2, 0)
     output_segments = np.array(output_segments).transpose(1, 2, 0)
     
-    return input_segments, output_segments
+    # Separate spline data into individual components
+    knots_input = [list(t[0][0]) for t in spline_data_input]
+    coeffs_input = [list(t[0][1]) for t in spline_data_input]
+    knots_output = [list(t[0][0]) for t in spline_data_output]
+    coeffs_output = [list(t[0][1]) for t in spline_data_output]
+
+    return input_segments, output_segments, knots_input, coeffs_input, knots_output, coeffs_output
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process drone trajectory data.')
@@ -47,7 +65,7 @@ if __name__ == "__main__":
     save_path = args.save_path
     
     df = read_trajectory(file_path)
-    input_segments, output_segments = process_trajectory(df, inp_seg_len, out_seg_len, compute_velocity)
+    input_segments, output_segments, knots_input, coeffs_input, knots_output, coeffs_output = process_trajectory(df, inp_seg_len, out_seg_len, compute_velocity)
     
     num_input_segments = input_segments.shape[2]
     num_output_segments = output_segments.shape[2]
@@ -55,11 +73,22 @@ if __name__ == "__main__":
     print(f"Number of input segments: {num_input_segments}")
     print(f"Number of output segments: {num_output_segments}")
     
-    # Save segments and additional information to .npz file
-    np.savez(save_path, 
-             input_segments=input_segments, 
-             output_segments=output_segments, 
-             num_input_segments=num_input_segments, 
-             num_output_segments=num_output_segments,
-             inp_seg_len=inp_seg_len,
-             out_seg_len=out_seg_len)
+    # Save segments, spline data, and additional information to .npz file
+    npz_data = {
+        'input_segments': input_segments,
+        'output_segments': output_segments,
+        'num_input_segments': num_input_segments,
+        'num_output_segments': num_output_segments,
+        'inp_seg_len': inp_seg_len,
+        'out_seg_len': out_seg_len
+    }
+    
+    for i in range(num_input_segments):
+        npz_data[f'knots_input_{i}'] = knots_input[i]
+        npz_data[f'coeffs_input_{i}'] = coeffs_input[i]
+        
+    for i in range(num_output_segments):
+        npz_data[f'knots_output_{i}'] = knots_output[i]
+        npz_data[f'coeffs_output_{i}'] = coeffs_output[i]
+    
+    np.savez(save_path, **npz_data)

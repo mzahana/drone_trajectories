19af44de260e624c46b467f18a221784e22e555b
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2023-11-26 15:20:45 +0300

    Update pkg

--- compute_stats.py
+++ compute_stats.py
@@ -34,7 +34,7 @@ import glob
 import sys
 import matplotlib.pyplot as plt
 
-def process_file(filename, pos_mean, pos_L, vel_mean, vel_L, pos_dir, vel_dir):
+def process_file(filename, pos_mean, pos_L, vel_mean, vel_L, max_pos_length, max_vel_magnitude, dirs):
     df = pd.read_csv(filename)
 
     # Calculate differences for velocities and velocity magnitudes
@@ -50,21 +50,76 @@ def process_file(filename, pos_mean, pos_L, vel_mean, vel_L, pos_dir, vel_dir):
     pos_data = df[['tx', 'ty', 'tz']].values
     pos_centered = pos_data - pos_mean
     transformed_pos = np.dot(pos_L, pos_centered.T).T
-    df_transformed_pos = pd.DataFrame(transformed_pos, columns=['tx', 'ty', 'tz'])
-    df_transformed_pos.insert(0, 'timestamp', df['timestamp'])  # Insert timestamp as the first column
+    df_transformed_white_pos = pd.DataFrame(transformed_pos, columns=['tx', 'ty', 'tz'])
+    df_transformed_white_pos.insert(0, 'timestamp', df['timestamp'])  # Insert timestamp as the first column
 
     # Whitening transformations for velocities
     vel_data = df_vel[['vx', 'vy', 'vz']].values
     vel_centered = vel_data - vel_mean
     transformed_vel = np.dot(vel_L, vel_centered.T).T
-    df_transformed_vel = pd.DataFrame(transformed_vel, columns=['vx', 'vy', 'vz'])
-    df_transformed_vel.insert(0, 'timestamp', df_vel['timestamp'])  # Insert adjusted timestamp for velocity
+    df_transformed_white_vel = pd.DataFrame(transformed_vel, columns=['vx', 'vy', 'vz'])
+    df_transformed_white_vel.insert(0, 'timestamp', df_vel['timestamp'])  # Insert adjusted timestamp for velocity
+
+    # Max normalization
+    normalized_pos_max = pos_data / max_pos_length
+    df_transformed_max_pos = pd.DataFrame(normalized_pos_max, columns=['tx', 'ty', 'tz'])
+    df_transformed_max_pos.insert(0, 'timestamp', df['timestamp'])
+
+    normalized_vel_max = vel_data / max_vel_magnitude
+    df_transformed_max_vel = pd.DataFrame(normalized_vel_max, columns=['vx', 'vy', 'vz'])
+    df_transformed_max_vel.insert(0, 'timestamp', df['timestamp'])
 
     # Save the transformed data
-    transformed_pos_filename = os.path.join(pos_dir, 'normalized_' + os.path.basename(filename))
-    transformed_vel_filename = os.path.join(vel_dir, 'normalized_vel_' + os.path.basename(filename))
-    df_transformed_pos.to_csv(transformed_pos_filename, index=False)
-    df_transformed_vel.to_csv(transformed_vel_filename, index=False)
+    save_transformed_data(df_transformed_white_pos, 'white_norm_pos_'+os.path.basename(filename), dirs['pos_whitening'])
+    save_transformed_data(df_transformed_white_vel, 'white_norm_vel_'+os.path.basename(filename), dirs['vel_whitening'])
+
+    # Save the max normalized data
+    save_transformed_data(df_transformed_max_pos, 'max_norm_pos_'+os.path.basename(filename), dirs['pos_max'])
+    save_transformed_data(df_transformed_max_vel, 'max_norm_vel_'+os.path.basename(filename), dirs['vel_max'])
+
+def save_transformed_data(data, filename, directory):
+    data.to_csv(os.path.join(directory, filename), index=False)
+
+def compute_stats(pos_df, vel_df, max_pos_length, max_vel_magnitude):
+    pos_stats = {
+        "input_mean": pos_df.mean().tolist(),
+        "input_std": pos_df.std().tolist(),
+        "cov_matrix": pos_df.cov().values,
+        "L_matrix": np.linalg.cholesky(np.linalg.inv(pos_df.cov())).tolist(),
+        "max_length": max_pos_length
+    }
+    vel_stats = {
+        "input_mean": vel_df.mean().tolist(),
+        "input_std": vel_df.std().tolist(),
+        "cov_matrix": vel_df.cov().values,
+        "L_matrix": np.linalg.cholesky(np.linalg.inv(vel_df.cov())).tolist(),
+        "max_velocity": max_vel_magnitude
+    }
+    return pos_stats, vel_stats
+
+def print_stats(pos_file, vel_file, pos_stats, vel_stats):
+    print(f"Files saved: {os.path.abspath(pos_file)} and {os.path.abspath(vel_file)}")
+    print("\nPosition Statistics:")
+    for key, value in pos_stats.items():
+        print(f"{key}: {value}")
+    print("\nVelocity Statistics:")
+    for key, value in vel_stats.items():
+        print(f"{key}: {value}")
+
+def plot_histograms(pos_df, vel_df, vel_mag_df):
+    fig, axs = plt.subplots(2, 2, figsize=(12, 10))
+
+    axs[0, 0].hist(pos_df['tx'], bins=30, color='blue', alpha=0.7)
+    axs[0, 0].set_title('Histogram of tx')
+    axs[0, 1].hist(pos_df['ty'], bins=30, color='green', alpha=0.7)
+    axs[0, 1].set_title('Histogram of ty')
+    axs[1, 0].hist(pos_df['tz'], bins=30, color='red', alpha=0.7)
+    axs[1, 0].set_title('Histogram of tz')
+    axs[1, 1].hist(vel_mag_df, bins=30, color='purple', alpha=0.7)
+    axs[1, 1].set_title('Histogram of Velocity Magnitude')
+
+    plt.tight_layout()
+    plt.show()
 
 def compute_stats_and_transform(directory):
     all_files = glob.glob(os.path.join(directory, "*.csv")) + glob.glob(os.path.join(directory, "*.txt"))
@@ -103,82 +158,56 @@ def compute_stats_and_transform(directory):
     pos_stats, vel_stats = compute_stats(combined_pos_df, combined_vel_df, max_pos_length, max_vel_magnitude)
 
     # Create directories for transformed data and determine the parent directory
+    # parent_dir = os.path.dirname(directory)
+    # transformed_pos_dir = os.path.join(parent_dir, 'transformed_position_data')
+    # transformed_vel_dir = os.path.join(parent_dir, 'transformed_velocity_data')
+    # os.makedirs(transformed_pos_dir, exist_ok=True)
+    # os.makedirs(transformed_vel_dir, exist_ok=True)
+
+    # Create directories
+    # parent_dir = os.path.join(directory, 'transformed_trajectories')
     parent_dir = os.path.dirname(directory)
-    transformed_pos_dir = os.path.join(parent_dir, 'transformed_position_data')
-    transformed_vel_dir = os.path.join(parent_dir, 'transformed_velocity_data')
-    os.makedirs(transformed_pos_dir, exist_ok=True)
-    os.makedirs(transformed_vel_dir, exist_ok=True)
+    # Create the transformed_trajectories directory in the parent directory
+    transformed_dir = os.path.join(parent_dir, 'transformed_trajectories')
+    os.makedirs(transformed_dir, exist_ok=True)
+    dirs = {
+        'pos_whitening': os.path.join(transformed_dir, 'pos_trajectories_whitening_normalization'),
+        'pos_max': os.path.join(transformed_dir, 'pos_trajectories_max_normalization'),
+        'vel_whitening': os.path.join(transformed_dir, 'vel_trajectories_whitening_normalization'),
+        'vel_max': os.path.join(transformed_dir, 'vel_trajectories_max_normalization')
+    }
+    for dir in dirs.values():
+        os.makedirs(dir, exist_ok=True)
 
     # Save the statistics in the parent directory
-    pos_stats_file = os.path.join(parent_dir, 'pos_stats.npz')
-    vel_stats_file = os.path.join(parent_dir, 'vel_stats.npz')
+    pos_stats_file = os.path.join(transformed_dir, 'pos_stats.npz')
+    vel_stats_file = os.path.join(transformed_dir, 'vel_stats.npz')
     np.savez(pos_stats_file, **pos_stats)
     np.savez(vel_stats_file, **vel_stats)
 
-    # Print and save statistics
-    print_and_save_stats(pos_stats_file, vel_stats_file, pos_stats, vel_stats)
+    # Print statistics
+    print_stats(pos_stats_file, vel_stats_file, pos_stats, vel_stats)
 
-    # Create directories for transformed data
-    parent_dir = os.path.dirname(directory)
-    transformed_pos_dir = os.path.join(parent_dir, 'transformed_position_data')
-    transformed_vel_dir = os.path.join(parent_dir, 'transformed_velocity_data')
-    os.makedirs(transformed_pos_dir, exist_ok=True)
-    os.makedirs(transformed_vel_dir, exist_ok=True)
-
+    
     # Process each file to transform and save data
     for filename in all_files:
         process_file(filename, pos_stats['input_mean'], pos_stats['L_matrix'], 
-                     vel_stats['input_mean'], vel_stats['L_matrix'], transformed_pos_dir, transformed_vel_dir)
+                     vel_stats['input_mean'], vel_stats['L_matrix'],
+                     max_pos_length, max_vel_magnitude,
+                     dirs)
+
 
     # Plot histograms of the original and velocity magnitude data
     plot_histograms(combined_pos_df, combined_vel_df, combined_vel_magnitude_df)
 
     # Print the paths to the saved npz files and the transformed directories
-    print(f"\npos_stats.npz saved at: {pos_stats_file}")
-    print(f"vel_stats.npz saved at: {vel_stats_file}")
-    print(f"Transformed position data directory: {transformed_pos_dir}")
-    print(f"Transformed velocity data directory: {transformed_vel_dir}")
-
-def compute_stats(pos_df, vel_df, max_pos_length, max_vel_magnitude):
-    pos_stats = {
-        "input_mean": pos_df.mean().tolist(),
-        "input_std": pos_df.std().tolist(),
-        "cov_matrix": pos_df.cov().values,
-        "L_matrix": np.linalg.cholesky(np.linalg.inv(pos_df.cov())).tolist(),
-        "max_length": max_pos_length
-    }
-    vel_stats = {
-        "input_mean": vel_df.mean().tolist(),
-        "input_std": vel_df.std().tolist(),
-        "cov_matrix": vel_df.cov().values,
-        "L_matrix": np.linalg.cholesky(np.linalg.inv(vel_df.cov())).tolist(),
-        "max_velocity": max_vel_magnitude
-    }
-    return pos_stats, vel_stats
-
-def print_and_save_stats(pos_file, vel_file, pos_stats, vel_stats):
-    print(f"Files saved: {os.path.abspath(pos_file)} and {os.path.abspath(vel_file)}")
-    print("\nPosition Statistics:")
-    for key, value in pos_stats.items():
+    print("Directories created and files saved:")
+    print(f"Parent directory: {transformed_dir}")
+    for key, value in dirs.items():
         print(f"{key}: {value}")
-    print("\nVelocity Statistics:")
-    for key, value in vel_stats.items():
-        print(f"{key}: {value}")
+    print(f"pos_stats.npz saved at: {os.path.join(transformed_dir, 'pos_stats.npz')}")
+    print(f"vel_stats.npz saved at: {os.path.join(transformed_dir, 'vel_stats.npz')}")
 
-def plot_histograms(pos_df, vel_df, vel_mag_df):
-    fig, axs = plt.subplots(2, 2, figsize=(12, 10))
-
-    axs[0, 0].hist(pos_df['tx'], bins=30, color='blue', alpha=0.7)
-    axs[0, 0].set_title('Histogram of tx')
-    axs[0, 1].hist(pos_df['ty'], bins=30, color='green', alpha=0.7)
-    axs[0, 1].set_title('Histogram of ty')
-    axs[1, 0].hist(pos_df['tz'], bins=30, color='red', alpha=0.7)
-    axs[1, 0].set_title('Histogram of tz')
-    axs[1, 1].hist(vel_mag_df, bins=30, color='purple', alpha=0.7)
-    axs[1, 1].set_title('Histogram of Velocity Magnitude')
-
-    plt.tight_layout()
-    plt.show()
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
